6eb6993b5fbf1a96c7c57dc0ba484b63afeba9de
Luiz Paulo Monteiro <lpsmonteiro@gmail.com>
2021-02-07 20:58:34 -0300

    Some more code refactoring

--- defectdojo_cli/engagements.py
+++ defectdojo_cli/engagements.py
@@ -17,16 +17,42 @@ class Engagements(object):
         close      Close an engagement (engagements close --help for more details)
         update     Update an engagement (engagements update --help for more details)
 ''')
-        parser.add_argument('sub_command', help='Sub_command to run')
-        args = parser.parse_args(sys.argv[2:3]) # Get sub_command
-        if not hasattr(self, args.sub_command):
-            print('Unrecognized sub_command')
-            parser.print_help()
-            exit(1)
-        # Use dispatch pattern to invoke method with same name
-        getattr(self, args.sub_command)()
+        parser.add_argument('sub_command', help='Sub_command to run',
+                            choices=['create', 'close', 'update'])
+        # Get sub_command
+        args = parser.parse_args(sys.argv[2:3])
+        # Use dispatch pattern to invoke method with same name (that starts with _)
+        getattr(self, '_'+args.sub_command)()
 
-    def create(self):
+    def create(self, **kwargs):
+        args = dict()
+        # Parse arguments
+        for key,value in kwargs.items():
+            args[key] = value
+        # Prepare JSON data to be send
+        request_json = dict()
+        API_URL = args['url']+'/api/v2'
+        ENGAGEMENTS_URL = API_URL+'/engagements/'
+        request_json['name'] = args['name']
+        request_json['description'] = args['desc']
+        request_json['product'] = args['product_id']
+        request_json['lead'] = args['lead_id']
+        request_json['target_start'] = args['start_date']
+        request_json['target_end'] = args['end_date']
+        request_json['engagement_type'] = args['type']
+        request_json['status'] = args['status']
+        if args['repo_url'] is not None:
+            request_json['source_code_management_uri'] = args['repo_url']
+        if args['branch_tag'] is not None:
+            request_json['branch_tag'] = args['branch_tag']
+        if args['product_version'] is not None:
+            request_json['version'] = args['product_version']
+        # Make the request
+        response = Util().request_apiv2('POST', ENGAGEMENTS_URL, args['api_key'], data=request_json)
+        return response
+
+    def _create(self):
+        # Read user-supplied arguments
         parser = argparse.ArgumentParser(description='Create an engagement on DefectDojo',
                                          usage='defectdojo engagements create [<args>]')
         optional = parser._action_groups.pop()
@@ -53,85 +79,52 @@ class Engagements(object):
                             metavar='TAG_OR_BRANCH')
         optional.add_argument('--product_version', help='Version of the product the engagement tested')
         parser._action_groups.append(optional)
-
-        request_json = dict()
         # Parse out arguments ignoring the first three (because we're inside a sub_command)
         args = vars(parser.parse_args(sys.argv[3:]))
-        API_URL = args['url']+'/api/v2'
-        ENGAGEMENTS_URL = API_URL+'/engagements/'
-        request_json['name'] = args['name']
-        request_json['description'] = args['desc']
-        request_json['product'] = args['product_id']
-        request_json['lead'] = args['lead_id']
-        request_json['target_start'] = args['start_date']
-        request_json['target_end'] = args['end_date']
-        request_json['engagement_type'] = args['type']
-        request_json['status'] = args['status']
-        if args['repo_url'] is not None:
-            request_json['source_code_management_uri'] = args['repo_url']
-        if args['branch_tag'] is not None:
-            request_json['branch_tag'] = args['branch_tag']
-        if args['product_version'] is not None:
-            request_json['version'] = args['product_version']
-
         # Create engagement
-        response = Util()._request('POST', ENGAGEMENTS_URL, args['api_key'], data=request_json)
-
+        response = self.create(**args)
         # Pretty print JSON response
         Util().default_output(response, sucess_status_code=201)
 
-    def close(self):
+    def close(self, **kwargs):
+        args = dict()
+        # Parse arguments
+        for key,value in kwargs.items():
+            args[key] = value
+        # Prepare parameters
+        API_URL = args['url']+'/api/v2'
+        ENGAGEMENTS_URL = API_URL+'/engagements/'
+        ENGAGEMENTS_ID_URL = ENGAGEMENTS_URL+args['engagement_id']
+        ENGAGEMENTS_CLOSE_URL = ENGAGEMENTS_ID_URL+'/close/'
+        # Make the request
+        response = Util().request_apiv2('POST', ENGAGEMENTS_CLOSE_URL, args['api_key'])
+        return response
+
+    def _close(self):
+        # Read user-supplied arguments
         parser = argparse.ArgumentParser(description='Close an engagement on DefectDojo',
                                          usage='defectdojo engagements close ENGAGEMENT_ID')
         required = parser.add_argument_group('required arguments')
         parser.add_argument('engagement_id', help='ID of the engagement to be closed')
         required.add_argument('--url', help='DefectDojo URL', required=True)
         required.add_argument('--api_key', help='API v2 Key', required=True)
-
-        request_json = dict()
         # Parse out arguments ignoring the first three (because we're inside a sub_command)
         args = vars(parser.parse_args(sys.argv[3:]))
-        API_URL = args['url']+'/api/v2'
-        ENGAGEMENTS_URL = API_URL+'/engagements/'
-        ENGAGEMENTS_ID_URL = ENGAGEMENTS_URL+args['engagement_id']
-        ENGAGEMENTS_CLOSE_URL = ENGAGEMENTS_ID_URL+'/close/'
-
         # Close engagement
-        response = Util()._request('POST', ENGAGEMENTS_CLOSE_URL, args['api_key'])
+        response = self.close(**args)
         # DefectDojo doesnt has an output when a engagement is successfully closed so we need to create one
-        type(response).text = PropertyMock(return_value='{"return": "sucess"}')
-
+        if response.status_code == 200:
+            type(response).text = PropertyMock(return_value='{"return": "sucess"}')
         # Pretty print JSON response
         Util().default_output(response, sucess_status_code=200)
 
-    def update(self):
-        parser = argparse.ArgumentParser(description='Update a engagement on DefectDojo',
-                                         usage='defectdojo engagements update ENGAGEMENT_ID [<args>]')
-        optional = parser._action_groups.pop()
-        required = parser.add_argument_group('required arguments')
-        parser.add_argument('engagement_id', help='ID of the engagement to be updated')
-        required.add_argument('--url', help='DefectDojo URL', required=True)
-        required.add_argument('--api_key', help='API v2 Key', required=True)
-        optional.add_argument('--name', help='Engagement name')
-        optional.add_argument('--desc', help='Engagement description', metavar='DESCRIPTION')
-        optional.add_argument('--product_id', help='ID of the product the engagement belongs to')
-        optional.add_argument('--lead_id', help='ID of the user responsible for this engagement')
-        optional.add_argument('--start_date', help='Engagement starting date', metavar='YYYY-MM-DD')
-        optional.add_argument('--end_date', help='Engagement ending date', metavar='YYYY-MM-DD')
-        optional.add_argument('--type', help='Engagement type', choices=['Interactive', 'CI/CD'])
-        optional.add_argument('--repo_url', help='Link to source code')
-        optional.add_argument('--branch_tag', help='Tag or branch of the product the engagement tested',
-                            metavar='TAG_OR_BRANCH')
-        optional.add_argument('--product_version', help='Version of the product the engagement tested')
-        optional.add_argument('--status', help='Engagement status',
-                            choices=['Not Started', 'Blocked', 'Cancelled', 'Completed', 'In Progress',
-                                     'On Hold', 'Waiting for Resource'])
-        parser._action_groups.append(optional)
-
+    def update(self, **kwargs):
+        args = dict()
+        # Parse arguments
+        for key,value in kwargs.items():
+            args[key] = value
+        # Prepare JSON data to be send
         request_json = dict()
-        # Parse out arguments ignoring the first three (because we're inside a sub_command)
-        # and prepare JSON data to be send
-        args = vars(parser.parse_args(sys.argv[3:]))
         API_URL = args['url']+'/api/v2'
         ENGAGEMENTS_URL = API_URL+'/engagements/'
         ENGAGEMENTS_ID_URL = ENGAGEMENTS_URL+args['engagement_id']+'/'
@@ -157,9 +150,36 @@ class Engagements(object):
             request_json['version'] = args['product_version']
         if args['status'] is not None:
             request_json['status'] = args['status']
+        # Make the request
+        response = Util().request_apiv2('PATCH', ENGAGEMENTS_ID_URL, args['api_key'], data=request_json)
 
+    def _update(self):
+        # Read user-supplied arguments
+        parser = argparse.ArgumentParser(description='Update a engagement on DefectDojo',
+                                         usage='defectdojo engagements update ENGAGEMENT_ID [<args>]')
+        optional = parser._action_groups.pop()
+        required = parser.add_argument_group('required arguments')
+        parser.add_argument('engagement_id', help='ID of the engagement to be updated')
+        required.add_argument('--url', help='DefectDojo URL', required=True)
+        required.add_argument('--api_key', help='API v2 Key', required=True)
+        optional.add_argument('--name', help='Engagement name')
+        optional.add_argument('--desc', help='Engagement description', metavar='DESCRIPTION')
+        optional.add_argument('--product_id', help='ID of the product the engagement belongs to')
+        optional.add_argument('--lead_id', help='ID of the user responsible for this engagement')
+        optional.add_argument('--start_date', help='Engagement starting date', metavar='YYYY-MM-DD')
+        optional.add_argument('--end_date', help='Engagement ending date', metavar='YYYY-MM-DD')
+        optional.add_argument('--type', help='Engagement type', choices=['Interactive', 'CI/CD'])
+        optional.add_argument('--repo_url', help='Link to source code')
+        optional.add_argument('--branch_tag', help='Tag or branch of the product the engagement tested',
+                            metavar='TAG_OR_BRANCH')
+        optional.add_argument('--product_version', help='Version of the product the engagement tested')
+        optional.add_argument('--status', help='Engagement status',
+                            choices=['Not Started', 'Blocked', 'Cancelled', 'Completed', 'In Progress',
+                                     'On Hold', 'Waiting for Resource'])
+        parser._action_groups.append(optional)
+        # Parse out arguments ignoring the first three (because we're inside a sub_command)
+        args = vars(parser.parse_args(sys.argv[3:]))
         # Update engagement
-        response = Util()._request('PATCH', ENGAGEMENTS_ID_URL, args['api_key'], data=request_json)
-
+        response = self.update(**args)
         # Pretty print JSON response
         Util().default_output(response, sucess_status_code=200)
--- defectdojo_cli/findings.py
+++ defectdojo_cli/findings.py
@@ -16,21 +16,45 @@ class Findings(object):
         list            List findings
         print_scanners  Print a list of possible entries for the --scanner flag
 ''')
-        parser.add_argument('sub_command', help='Sub_command to run')
-        args = parser.parse_args(sys.argv[2:3]) # Get sub_command
-        if not hasattr(self, args.sub_command):
-            print('Unrecognized sub_command')
-            parser.print_help()
-            exit(1)
-        # Use dispatch pattern to invoke method with same name
-        getattr(self, args.sub_command)()
+        parser.add_argument('sub_command', help='Sub_command to run',
+                            choices=['upload', 'list', 'print_scanners'])
+        # Get sub_command
+        args = parser.parse_args(sys.argv[2:3])
+        # Use dispatch pattern to invoke method with same name (that starts with _)
+        getattr(self, '_'+args.sub_command)()
 
     def print_scanners(self):
         for scanner in Util().ACCEPTED_SCANS:
             print(scanner)
         exit(0)
 
-    def upload(self):
+    def upload(self, **kwargs):
+        args = dict()
+        # Parse arguments
+        for key,value in kwargs.items():
+            args[key] = value
+        # Prepare JSON data to be send
+        request_json = dict()
+        API_URL = args['url']+'/api/v2'
+        IMPORT_SCAN_URL = API_URL+'/import-scan/'
+        request_json['scan_date'] = datetime.now().strftime('%Y-%m-%d')
+        request_json['scan_type'] = args['scanner']
+        request_json['verified'] = args['verified']
+        request_json['engagement'] = args['engagement_id']
+        request_json['lead'] = args['lead_id']
+        request_json['active'] = args['active']
+        request_json['minimum_severity'] = args['min_severity']
+        if args['tag'] is not None:
+            request_json['tags'] = args['tag']
+        # Prepare file data to be send
+        files = dict()
+        files['file'] = open(args['result_file'])
+        # Make request
+        response = Util().request_apiv2('POST', IMPORT_SCAN_URL, args['api_key'], files=files, data=request_json)
+        return response
+
+    def _upload(self):
+        # Read user-supplied arguments
         parser = argparse.ArgumentParser(description='Upload findings (scan results) to DefectDojo',
                                          usage='defectdojo findings upload RESULT_FILE [<args>]')
         optional = parser._action_groups.pop()
@@ -43,79 +67,41 @@ class Findings(object):
         required.add_argument('--engagement_id', help='Engagement ID', required=True)
         required.add_argument('--lead_id', help='ID of the user conducting the operation', required=True)
         optional.add_argument('--active', help='Mark vulnerabilities found as active (default)',
-                          action='store_true', dest='active')
+                              action='store_true', dest='active')
         optional.add_argument('--inactive', help='Mark vulnerabilities found as inactive',
-                          action='store_false', dest='active')
+                              action='store_false', dest='active')
         optional.add_argument('--verified', help='Mark vulnerabilities found as verified',
-                          action='store_true', dest='verified')
+                              action='store_true', dest='verified')
         optional.add_argument('--unverified', help='Mark vulnerabilities found as unverified (default)',
-                          action='store_false', dest='verified')
+                              action='store_false', dest='verified')
         optional.set_defaults(active=True, verified=False)
         optional.add_argument('--min_severity', help='Ignore findings below this severity (default = "Low")',
-                          choices=['Informational', 'Low', 'Medium', 'High', 'Critical'], default='Low')
+                              choices=['Informational', 'Low', 'Medium', 'High', 'Critical'], default='Low')
         optional.add_argument('--tag', help='Scanner tag (can be used multiple times)', action='append')
+        optional.add_argument('--auto_close', help='Close all the open findings from --scanner and '+
+                              '--engagement_id that are not listed in this upload (requires deduplication)',
+                              action='store_true', default=False)
         parser._action_groups.append(optional)
-
         # Parse out arguments ignoring the first three (because we're inside a sub-command)
-        # and prepare JSON data to be send
-        request_json = dict()
         args = vars(parser.parse_args(sys.argv[3:]))
-        API_URL = args['url']+'/api/v2'
-        IMPORT_SCAN_URL = API_URL+'/import-scan/'
-        request_json['scan_date'] = datetime.now().strftime('%Y-%m-%d')
-        request_json['scan_type'] = args['scanner']
-        request_json['verified'] = args['verified']
-        request_json['engagement'] = args['engagement_id']
-        request_json['lead'] = args['lead_id']
-        request_json['active'] = args['active']
-        request_json['minimum_severity'] = args['min_severity']
-        if args['tag'] is not None:
-            request_json['tags'] = args['tag']
+        # Upload results
+        response = self._upload(**args)
+        # Print output
+        if args['auto_close']:
+            if response.status_code == 201: #Success
+                # Pretty print JSON response
+                Util().default_output(response, sucess_status_code=201)
+        else:
+            # Pretty print JSON response
+            Util().default_output(response, sucess_status_code=201)
 
-        # Prepare file data to be send
-        files = dict()
-        files['file'] = open(args['result_file'])
-
-        # Upload findings
-        response = Util()._request('POST', IMPORT_SCAN_URL, args['api_key'], files=files, data=request_json)
-
-        # Pretty print JSON response
-        Util().default_output(response, sucess_status_code=201)
-
-    def list(self):
-        parser = argparse.ArgumentParser(description='List findings stored on DefectDojo',
-                                         usage='defectdojo findings list [<args>]')
-        optional = parser._action_groups.pop()
-        required = parser.add_argument_group('required arguments')
-        required.add_argument('--url', help='DefectDojo URL', required=True)
-        required.add_argument('--api_key', help='API v2 Key', required=True)
-        optional.add_argument('--id', help='Get finding with this id')
-        optional.add_argument('--product_id', help='Filter by product')
-        optional.add_argument('--engagement_id', help='Filter by engagement')
-        optional.add_argument('--scanner', help='Filter by scanner',
-                              choices=Util().ACCEPTED_SCANS, metavar='SCANNER')
-        optional.add_argument('--active', help='List only actives findings',
-                            action='store_true', dest='active')
-        optional.add_argument('--inactive', help='List only inactives findings',
-                            action='store_false', dest='active')
-        optional.add_argument('--valid', help='List only valid findings (true-positives)',
-                            action='store_true', dest='valid')
-        optional.add_argument('--false_positives', help='List only false_positives findings',
-                            action='store_false', dest='valid')
-        optional.add_argument('--in_scope', help='List only findings in-scope',
-                            action='store_true', dest='scope')
-        optional.add_argument('--out_of_scope', help='List only findings out-of-scope',
-                            action='store_false', dest='scope')
-        optional.add_argument('--json', help='Print output in JSON format', action='store_true', default=False)
-        optional.add_argument('--limit', help='Number of results to return (by default it gets all the findings)')
-        optional.add_argument('--offset', help='The initial index from which to return the results (not needed if the --limit flag is not set)')
-        optional.set_defaults(active=None, valid=None, scope=None)
-        parser._action_groups.append(optional)
-
-
+    def list(self, **kwargs):
+        args = dict()
+        # Parse arguments
+        for key,value in kwargs.items():
+            args[key] = value
+        # Create parameters to be requested
         request_params = dict()
-        # Parse out arguments ignoring the first three (because we're inside a sub-command)
-        args = vars(parser.parse_args(sys.argv[3:]))
         API_URL = args['url']+'/api/v2'
         FINDINGS_URL = API_URL+'/findings/'
         if args['id'] is not None:
@@ -129,7 +115,7 @@ class Findings(object):
             temp_params = dict()
             temp_params['name'] = args['scanner']
             # Make a get request to /test_types passing the scanner as parameter
-            temp_response = Util()._request('GET', API_URL+'/test_types/', args['api_key'], params=temp_params)
+            temp_response = Util().request_apiv2('GET', API_URL+'/test_types/', args['api_key'], params=temp_params)
             # Tranform the above response in json and get the id
             scanner_id = json.loads(temp_response.text)['results'][0]['id']
             # Add to request_params
@@ -153,18 +139,55 @@ class Findings(object):
             request_params['limit'] = args['limit']
         else:
             # Make a request to API getting only one finding to retrieve the total amount of findings
-            request_params['limit'] = 1
-            temp_response = Util()._request('GET', FINDINGS_URL, args['api_key'], params=request_params)
+            temp_params = args.copy()
+            temp_params['limit'] = 1
+            temp_response = self.list(**temp_params)
             limit = int(json.loads(temp_response.text)['count'])
             request_params['limit'] = limit
+        # Make request
+        response = Util().request_apiv2('GET', FINDINGS_URL, args['api_key'], params=request_params)
+        return response
 
+    def _list(self):
+        # Read user-supplied arguments
+        parser = argparse.ArgumentParser(description='List findings stored on DefectDojo',
+                                         usage='defectdojo findings list [<args>]')
+        optional = parser._action_groups.pop()
+        required = parser.add_argument_group('required arguments')
+        required.add_argument('--url', help='DefectDojo URL', required=True)
+        required.add_argument('--api_key', help='API v2 Key', required=True)
+        optional.add_argument('--id', help='Get finding with this id')
+        optional.add_argument('--product_id', help='Filter by product')
+        optional.add_argument('--engagement_id', help='Filter by engagement')
+        optional.add_argument('--scanner', help='Filter by scanner',
+                              choices=Util().ACCEPTED_SCANS, metavar='SCANNER')
+        optional.add_argument('--active', help='List only actives findings',
+                            action='store_true', dest='active')
+        optional.add_argument('--inactive', help='List only inactives findings',
+                            action='store_false', dest='active')
+        optional.add_argument('--valid', help='List only valid findings (true-positives)',
+                            action='store_true', dest='valid')
+        optional.add_argument('--false_positives', help='List only false-positives findings',
+                            action='store_false', dest='valid')
+        optional.add_argument('--in_scope', help='List only findings in-scope',
+                            action='store_true', dest='scope')
+        optional.add_argument('--out_of_scope', help='List only findings out-of-scope',
+                            action='store_false', dest='scope')
+        optional.add_argument('--json', help='Print output in JSON format', action='store_true', default=False)
+        optional.add_argument('--limit',
+                              help='Number of results to return (by default it gets all the findings)')
+        optional.add_argument('--offset', help='The initial index from which to return the results '
+                              +'(not needed if the --limit flag is not set)')
+        optional.set_defaults(active=None, valid=None, scope=None)
+        parser._action_groups.append(optional)
+        # Parse out arguments ignoring the first three (because we're inside a sub-command)
+        args = vars(parser.parse_args(sys.argv[3:]))
         # Get findings
-        response = Util()._request('GET', FINDINGS_URL, args['api_key'], params=request_params)
-
+        response = self.list(**args)
+        # Print output
         json_out = json.loads(response.text)
         if response.status_code == 200: # Sucess
-            # Print output
-            if args['json'] is True:
+            if args['json'] is True: # If --json flag is used
                 # Pretty print output in json
                 pretty_json_out = json.dumps(json_out, indent=4)
                 print(pretty_json_out)
@@ -175,7 +198,6 @@ class Findings(object):
                 mediums = []
                 lows = []
                 infos = []
-
                 for finding in json_out['results']:
                     if finding['severity'] == 'Critical':
                         criticals.append(finding)
@@ -187,7 +209,6 @@ class Findings(object):
                         lows.append(finding)
                     if finding['severity'] == 'Info':
                         infos.append(finding)
-
                 if criticals:
                     print('#################### CRITICAL ####################')
                     for finding in criticals:
