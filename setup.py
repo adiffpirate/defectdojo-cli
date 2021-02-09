2737a8e63ba6b580ade9951db75c45e5e9f140a8
adiffpirate <lpsmonteiro@gmail.com>
2021-02-09 16:12:22 -0300

    Some adjustments

--- setup.py
+++ setup.py
@@ -4,13 +4,6 @@ except ImportError:
     from distutils.core import setup
 
 setup(
-    name='defectdojo_cli',
-    version='0.2',
-    description='CLI Wrapper for DefectDojo using APIv2',
-    url='https://github.com/adiffpirate/defectdojo-cli',
-    author='Luiz Paulo Souto Monteiro',
-    author_email='lpsmonteiro2@gmail.com',
-    license='MIT',
     packages=['defectdojo_cli'],
     install_requires=['requests'],
     entry_points = {
